63c3ad3d04f75d42db6083ffcbfba219ed32b028
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-03-25 20:37:30 -0300

    Criação de variaveis

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int idade = 10;
+string nomeCliente = "Danton Ferrari";
+decimal preco = 20.4m;
+char letra = 'a';
+var nomeCompleto = "Danton Gabriel Neis Ferrari";
+var altura = 180;
