c1af81ac2b6b8fd87c89ca92f0fb7d0772e19bd6
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-03-25 21:43:43 -0300

    Add variaveis e tipos de console: Write/WriteLine/ReadLine/ReadKey

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -4,3 +4,13 @@ decimal preco = 20.4m;
 char letra = 'a';
 var nomeCompleto = "Danton Gabriel Neis Ferrari";
 var altura = 180;
+
+Console.Write("Digite seu nome: ");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine("Bem vindo " + nomeCompleto);
+Console.WriteLine();
+Console.Write("Digite sua idade: ");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("Idade informada: " + idade);
+Console.ReadKey();
